12feed95993d406b697b4491773077410dc0ad39
Dikrof <annanedavnyaya@yandex.ru>
2023-07-27 18:42:12 +0300

    tests passed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat baen2 = (Cat) applicationContext.getBean("cat");
+        Cat baen3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean.equals(bean1));
+        System.out.println(baen2.equals(baen3));
+
     }
 }
